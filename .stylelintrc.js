0abb0265023df83552436c67e9e88ccb18175c01
onpp <10293030+onpp@users.noreply.github.com>
2022-07-29 06:33:02 +0900

    add Storybook (not setting)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
-  ignoreFiles: ['**/node_modules/**'],
+  ignoreFiles: ['**/node_modules/**', '**/stories/**'],
   rules: {
     'string-quotes': 'single',
   },
