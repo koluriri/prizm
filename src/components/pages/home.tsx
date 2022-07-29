6586d39828648c2a3796c2fd4fd4c2e705c767e6
onpp <10293030+onpp@users.noreply.github.com>
2022-07-29 13:32:58 +0900

    user name setting test

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,12 +1,30 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import OnlineUsers from 'components/organisms/onlineusers';
 
 const Home: FC<{ setGame: () => void }> = ({ setGame }) => {
   const info = css({
     background: '#efefef',
   });
+
+  const getUName = (): string => {
+    const userName = localStorage.getItem('prizm-username');
+    if (!userName) {
+      const inputName = window.prompt('ユーザー名を入力してください', '');
+      if (inputName) {
+        localStorage.setItem('prizm-username', inputName);
+
+        return inputName;
+      }
+
+      return '海ネズミ';
+    }
+
+    return userName;
+  };
+  const userName = useMemo(() => getUName(), []);
+  console.log(userName);
 
   return (
     <>
