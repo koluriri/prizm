45c4e8e4d79e3f6797333b6a5360239f181bcd12
onpp <10293030+onpp@users.noreply.github.com>
2022-08-26 07:50:30 +0900

    onlineusers designed

--- src/components/organisms/onlineusers.tsx
+++ src/components/organisms/onlineusers.tsx
@@ -1,34 +1,131 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { Users } from 'data/types';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { useSelector } from 'react-redux';
 import { RootState } from 'ducks/rootReducer';
 
 const OnlineUsers: FC<{
   users: Users | undefined;
-}> = ({ users }) => {
+  editMode: () => void;
+}> = ({ users, editMode }) => {
   const me = useSelector((state: RootState) => state.user.key);
+  const [usersLength, setUsersLength] = useState(0);
+  useEffect(() => {
+    if (users) {
+      setUsersLength(
+        Object.keys(users).filter(
+          (key) => Date.now() - users[key].pingStamp < 3500,
+        ).length,
+      );
+    } else {
+      setUsersLength(0);
+    }
+  }, [users]);
 
   return (
-    <>
-      <p>オンラインのユーザー：</p>
-      <ul>
+    <div
+      css={css(`
+    border: 4px solid var(--primary-color);
+    border-radius: 20px;
+    padding: 15px 20px;
+    backdrop-filter: blur(30px);
+    min-width: 320px;
+    transition: 0.2s;`)}
+    >
+      <div
+        css={css(`
+    font-size: 15px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: 700;`)}
+      >
+        {usersLength === 0 && <span>接続中…</span>}
+        {usersLength !== 0 && (
+          <span>
+            現在、
+            <em>
+              <span>{usersLength}</span>人
+            </em>
+            が接続中
+          </span>
+        )}
+        {usersLength !== 0 && (
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => editMode()}
+          >
+            名前を変更する
+          </button>
+        )}
+      </div>
+      <ul
+        css={css(`
+    margin: 0;
+    padding: 25px 23px 21px;
+    list-style-type: none;
+    min-height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;`)}
+      >
         {users &&
           Object.keys(users).map(
             (key) =>
               Date.now() - users[key].pingStamp < 3500 && (
-                <li key={key}>
-                  <span css={css(`color:${users[key].color}`)}>●</span>
-                  {users[key].userName}
-                  {key === me && ' (あなた) '}
-                  スコア:{users[key].score || '0'}
+                <li
+                  key={key}
+                  css={css(`
+                  margin: 7px 0;
+                  --user-color: ${users[key].color};
+                  display: flex;
+                  flex-direction: row;
+                  align-items: center;
+                  font-weight: 700;
+                  line-height: 1;
+                  flex-wrap: wrap;
+
+                  &:before {
+                    content: '';
+                    width: 10px;
+                    height: 10px;
+                    display: block;
+                    background: var(--user-color);
+                    border-radius: 10px;
+                    margin-right: 7px;
+                  }
+                  `)}
+                >
+                  <span>{users[key].userName}</span>
+                  <span
+                    css={css(`
+                      margin: 0 7px;
+                      font-size: 13px;
+                      font-weight: 600;
+                      opacity: 0.6;
+                  `)}
+                  >
+                    スコア{users[key].score || '0'}
+                  </span>
+                  <span
+                    css={css(`
+                    font-size: 13px;
+                    font-weight: 600;
+                    color: var(--red);
+                  `)}
+                  >
+                    {key === me && 'この端末'}
+                  </span>
                 </li>
               ),
           )}
+        {usersLength === 0 && <span className="loading" />}
       </ul>
-    </>
+    </div>
   );
 };
 
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -4,15 +4,16 @@ import { FC, useEffect, useRef, useState } from 'react';
 
 import OnlineUsers from 'components/organisms/onlineusers';
 import GameSetter from 'components/organisms/gamesetter';
-import { localUserNameKey, Users } from 'data/types';
+import { Users } from 'data/types';
 import { listenUsers, updatePingStamp } from 'utils/database';
 
 import { useSelector } from 'react-redux';
 import { RootState } from 'ducks/rootReducer';
-import { initialUserName } from 'ducks/user';
+
+import bg from 'assets/home-bg-preview.svg';
+import logo from 'assets/logo-vertical.svg';
 
 const Home: FC<{ editMode: () => void }> = ({ editMode }) => {
-  const userName = localStorage.getItem(localUserNameKey) || initialUserName;
   const userKey = useSelector((state: RootState) => state.user.key);
 
   const [users, setUsers] = useState<Users>();
@@ -34,32 +35,63 @@ const Home: FC<{ editMode: () => void }> = ({ editMode }) => {
 
   return (
     <div
-      css={css(`display: grid;
-    grid-template-columns: 0.5fr 280px 2fr;
-    grid-template-areas:
-        "bg logo users"
-        "bg setter users";
-    min-height: 100vh;
-    width: 100vw;`)}
+      css={css(`
+      display: grid;
+      grid-template-columns: 1fr 280px 3fr;
+      grid-template-areas:
+          "bg logo users"
+          "bg setter users";
+      min-height: 100vh;
+      width: 100vw;
+      background-color: var(--bg-color);
+    `)}
     >
-      <div css={css(`grid-area: bg;`)}>area 1</div>
-      <div css={css(`grid-area: logo;`)}>
-        <h1>Prizm</h1>
+      <div
+        css={css(`grid-area: bg;
+      background-image: url(${bg});
+      background-repeat: repeat-x;
+      background-size: cover;
+      background-position: right center;`)}
+      />
+      <div
+        css={css(`grid-area: logo;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: end;`)}
+      >
+        <h1 css={css(`width: 120px;margin: 0;`)}>
+          <img src={logo} alt="prizm" />
+        </h1>
+        <h2
+          css={css(`
+    font-size: 17px;
+    font-weight: 900;
+    line-height: 1;
+    margin: 8px 0 10px;
+    text-align: center;
+    line-height: 1.35;`)}
+        >
+          市町村から
+          <br />
+          都道府県を当てるだけ！
+        </h2>
       </div>
-      <div css={css(`grid-area: users;`)}>
-        <p css={css({ background: '#efefef' })}>参加者を待機中</p>
-
-        <OnlineUsers users={users} />
+      <div
+        css={css(`
+      grid-area: users;background-image: url(${bg});
+      background-repeat: repeat-x;
+      background-size: cover;
+      background-position: left center;
+      display: flex;
+      align-items: center;
+      justify-content: start;
+      padding: 0 30px 0 10vw;`)}
+      >
+        <OnlineUsers users={users} editMode={() => editMode()} />
       </div>
       <div css={css(`grid-area: setter;`)}>
         <GameSetter users={users} />
-
-        <p>
-          {userName}としてプレイ中{' '}
-          <button type="button" onClick={() => editMode()}>
-            名前を変更
-          </button>
-        </p>
       </div>
     </div>
   );
