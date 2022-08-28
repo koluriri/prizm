810822047161658c7988bcfd4c07df3eaac7c721
onpp <10293030+onpp@users.noreply.github.com>
2022-08-29 03:58:04 +0900

    footer designed

--- src/components/templates/prizmfooter.tsx
+++ src/components/templates/prizmfooter.tsx
@@ -2,6 +2,9 @@
 import { css } from '@emotion/react';
 import { FC } from 'react';
 
+import { ReactComponent as Logo } from 'assets/logo-horizontal.svg';
+import { FaTwitter } from 'react-icons/fa';
+
 const PrizmFooter: FC = () => (
   <footer
     css={css`
@@ -10,13 +13,30 @@ const PrizmFooter: FC = () => (
       justify-content: center;
       align-items: center;
       flex-direction: column;
+      flex-direction: row;
+      line-height: 1;
+      gap: 8px;
 
       @media (min-height: 680px) {
         margin-top: -50px;
       }
     `}
   >
-    prizm
+    <Logo
+      css={css`
+        height: 18px;
+      `}
+    />
+    <p
+      css={css`
+        font-size: 12px;
+      `}
+    >
+      © 2022 koluriri.
+    </p>
+    <a href="//twitter.com/koluriri" target="_blank" rel="noreferrer">
+      <FaTwitter />
+    </a>
   </footer>
 );
 
