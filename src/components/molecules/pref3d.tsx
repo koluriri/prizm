33ec3c8b6bc5d451902b5c7011fe4e896e4846be
onpp <10293030+onpp@users.noreply.github.com>
2022-08-31 16:14:30 +0900

    split questioner

---
+++ src/components/molecules/pref3d.tsx
@@ -0,0 +1,98 @@
+/** @jsxImportSource @emotion/react */
+import { css, keyframes } from '@emotion/react';
+import { PrefectureStr } from 'data/types';
+import { FC } from 'react';
+
+import Prefsvg from 'components/atoms/prefsvg';
+import Ztext from 'react-ztext';
+
+const Pref3D: FC<{
+  name: PrefectureStr;
+}> = ({ name }) => (
+  <div
+    css={css`
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      & > div > span {
+        animation: 8s linear 0s infinite alternate ${keyframes`
+            0% {
+              transform: rotateY(-50deg);
+            }
+            100% {
+              transform: rotateY(50deg);
+            }
+          `};
+      }
+
+      & > div > span > span:last-of-type {
+        filter: drop-shadow(3px 10px 16px rgba(50, 8, 10, 0.1));
+      }
+
+      & > div > span > span:not(:first-of-type) {
+        filter: brightness(0.72);
+      }
+
+      & svg {
+        max-height: 26vh;
+        width: 100%;
+        padding: 10px;
+
+        & > *:not(defs) {
+          animation: 1s ease 0.7s 1 both ${keyframes`
+              0% {
+                opacity:0;
+                transform: translateY(220px);
+              }
+              1% {
+                opacity:1;
+              }
+              45% {
+                transform: translateY(-70px);
+              }
+              55% {
+                transform: translateY(0);
+              }
+              75% {
+                transform: translateY(-25px);
+              }
+              85% {
+                transform: translateY(0);
+              }
+              93% {
+                transform: translateY(-10px);
+              }
+              100% {
+                transform: translateY(0);
+              }
+            `};
+
+          &:nth-of-type(2n) {
+            animation-delay: 0.8s;
+            transform: translateZ(2000px);
+          }
+          &:nth-of-type(5n) {
+            animation-delay: 0.9s;
+          }
+        }
+      }
+    `}
+  >
+    <Ztext
+      depth="70px"
+      direction="both"
+      event="none"
+      eventRotation="30deg"
+      eventDirection="default"
+      fade={false}
+      perspective="270px"
+      layers={20}
+    >
+      <Prefsvg name={name} />
+    </Ztext>
+  </div>
+);
+
+export default Pref3D;
--- src/components/templates/questioner.tsx
+++ src/components/templates/questioner.tsx
@@ -1,73 +1,27 @@
 /** @jsxImportSource @emotion/react */
-import { css, keyframes } from '@emotion/react';
-import { FC, useRef, useEffect } from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
+import { css } from '@emotion/react';
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
 import { RootState } from 'ducks/rootReducer';
-import { gameSlice } from 'ducks/game';
+import { modesConvert, modesDetail } from 'data/types';
+import useQuestionTimer from 'hooks/use-questiontimer';
 
 import BigQuestion from 'components/organisms/bigquestion';
 import QuestionList from 'components/organisms/questionlistcontainer';
-import Prefsvg from 'components/atoms/prefsvg';
-import { modesConvert, modesDetail } from 'data/types';
-import { pushMessage } from 'utils/database';
-import { prefectureABC } from 'data/prefecture';
-
-import Ztext from 'react-ztext';
-import { getSummary, updateSummaryFromKey } from 'utils/summary';
+import AnswerDisplay from 'components/organisms/answerdisplay';
 
 const Questioner: FC<{
   finishGame: () => void;
 }> = ({ finishGame }) => {
   const gameObj = useSelector((state: RootState) => state.game.entity);
-  const gameKey = useSelector((state: RootState) => state.game.key);
   const isDuringGame = useSelector(
     (state: RootState) => state.game.isDuringGame,
   );
-
   const currentQuesIndex = useSelector(
     (state: RootState) => state.game.currentQuesIndex,
   );
-  const dispatch = useDispatch();
-  const { proceedQuesIndex } = gameSlice.actions;
 
-  const timerId = useRef<NodeJS.Timeout>();
-  const clearTimer = () => clearInterval(timerId.current);
-
-  useEffect(() => {
-    if (isDuringGame) {
-      const timerSeconds = 1.5;
-      timerId.current = setInterval(
-        () => dispatch(proceedQuesIndex()),
-        timerSeconds * 1000,
-      );
-    }
-
-    return clearTimer;
-  }, [isDuringGame, dispatch, proceedQuesIndex]);
-
-  // currentQuesIndexが変わるたびに実行。全部回したら負け処理
-  useEffect(() => {
-    if (currentQuesIndex === 2) {
-      const summary = getSummary();
-      if (summary && summary.lastPlay !== gameObj?.created) {
-        updateSummaryFromKey('playCount', (count) => count + 1);
-        updateSummaryFromKey('lastPlay', gameObj?.created ?? Date.now());
-      }
-    }
-    if (
-      gameObj &&
-      currentQuesIndex === gameObj.questions.length &&
-      gameObj.questions.length !== 0
-    ) {
-      pushMessage(gameKey, {
-        type: 'end',
-        value: '誰も答えられませんでした',
-      });
-      finishGame();
-      clearTimer();
-    }
-  }, [currentQuesIndex, gameObj, gameKey, finishGame]);
+  useQuestionTimer(finishGame);
 
   const displayQuestions =
     gameObj &&
@@ -76,162 +30,37 @@ const Questioner: FC<{
         ? modesConvert[gameObj.mode](question)
         : modesConvert.easy(question),
     );
-  const displayQuestion =
-    displayQuestions && displayQuestions[currentQuesIndex - 1];
+
+  const questionerContainer = css`
+    grid-area: questioner;
+    width: 100%;
+    padding-right: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
+  const modeDisp = css`
+    font-size: 15px;
+    font-weight: 700;
+    text-align: center;
+  `;
 
   return (
-    <div
-      className="questioner"
-      css={css`
-        grid-area: questioner;
-        width: 100%;
-        padding-right: 10px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-      `}
-    >
+    <div className="questioner" css={questionerContainer}>
       {isDuringGame && (
-        <div
-          css={css`
-            font-size: 15px;
-            font-weight: 700;
-            text-align: center;
-          `}
-        >
-          {gameObj && modesDetail[gameObj.mode]}
-        </div>
+        <div css={modeDisp}>{!!gameObj && modesDetail[gameObj.mode]}</div>
       )}
 
-      {isDuringGame && displayQuestion && (
-        <BigQuestion displayQuestion={displayQuestion} />
+      {isDuringGame && !!displayQuestions && (
+        <BigQuestion displayQuestion={displayQuestions[currentQuesIndex - 1]} />
       )}
 
-      {!isDuringGame && (
-        <>
-          <h3
-            css={css`
-              font-size: 38px;
-              text-align: center;
-              letter-spacing: -1px;
-              font-weight: 700;
-              margin-top: 0;
-              margin-bottom: 10px;
-
-              @media (max-width: 374px) {
-                font-size: 28px;
-              }
-
-              @media (min-width: 768px) {
-                letter-spacing: 1px;
-              }
-            `}
-          >
-            {gameObj?.answer}
-            <span
-              css={css`
-                display: block;
-                font-weight: 700;
-                font-size: 13px;
-                letter-spacing: 4px;
-                margin-top: 5px;
-                color: ${gameObj?.color};
-              `}
-            >
-              {!!gameObj?.answer && prefectureABC[gameObj.answer]}
-            </span>
-          </h3>
-          {!!gameObj?.answer && (
-            <div
-              css={css`
-                width: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-
-                & > div > span {
-                  animation: 8s linear 0s infinite alternate ${keyframes`
-                    0% {
-                      transform: rotateY(-50deg);
-                    }
-                    100% {
-                      transform: rotateY(50deg);
-                    }
-                  `};
-                }
-
-                & > div > span > span:last-of-type {
-                  filter: drop-shadow(3px 10px 16px rgba(50, 8, 10, 0.1));
-                }
-
-                & > div > span > span:not(:first-of-type) {
-                  filter: brightness(0.72);
-                }
-
-                & svg {
-                  max-height: 26vh;
-                  width: 100%;
-                  padding: 10px;
-
-                  & > *:not(defs) {
-                    animation: 1s ease 0.7s 1 both ${keyframes`
-                      0% {
-                        opacity:0;
-                        transform: translateY(220px);
-                      }
-                      1% {
-                        opacity:1;
-                      }
-                      45% {
-                        transform: translateY(-70px);
-                      }
-                      55% {
-                        transform: translateY(0);
-                      }
-                      75% {
-                        transform: translateY(-25px);
-                      }
-                      85% {
-                        transform: translateY(0);
-                      }
-                      93% {
-                        transform: translateY(-10px);
-                      }
-                      100% {
-                        transform: translateY(0);
-                      }
-                    `};
-
-                    &:nth-of-type(2n) {
-                      animation-delay: 0.8s;
-                      transform: translateZ(2000px);
-                    }
-                    &:nth-of-type(5n) {
-                      animation-delay: 0.9s;
-                    }
-                  }
-                }
-              `}
-            >
-              <Ztext
-                depth="70px"
-                direction="both"
-                event="none"
-                eventRotation="30deg"
-                eventDirection="default"
-                fade={false}
-                perspective="270px"
-                layers={20}
-              >
-                <Prefsvg name={gameObj.answer} />
-              </Ztext>
-            </div>
-          )}
-        </>
+      {!isDuringGame && !!gameObj?.answer && (
+        <AnswerDisplay answer={gameObj.answer} color={gameObj.color} />
       )}
 
-      {displayQuestions && (
+      {!!displayQuestions && (
         <QuestionList questions={displayQuestions} current={currentQuesIndex} />
       )}
     </div>
