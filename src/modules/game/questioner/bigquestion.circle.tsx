f9ab1a5d77d366b43f139da4bba7244f8b610c9a
onpp <10293030+onpp@users.noreply.github.com>
2022-09-05 10:57:02 +0900

    #61 デザイン調整

--- src/modules/game/questioner/bigquestion.circle.tsx
+++ src/modules/game/questioner/bigquestion.circle.tsx
@@ -1,68 +1,83 @@
 /** @jsxImportSource @emotion/react */
 import { css, keyframes } from '@emotion/react';
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'ducks/rootReducer';
 
 const BigQuestionCircle: FC<{
   size: number;
-}> = ({ size }) => (
-  <>
-    <span
-      css={css`
-        position: absolute;
-        content: '';
-        display: block;
-        top: 0;
-        left: 0;
-        width: ${size}px;
-        height: ${size}px;
-        border-radius: 100%;
-        transform: scale(1.05);
-        animation: 1.5s linear 0s infinite normal ${keyframes`
+}> = ({ size }) => {
+  const gameColor = useSelector(
+    (state: RootState) => state.game.entity?.color ?? 'var(--bg-color)',
+  );
+  const currentQuesIndex = useSelector(
+    (state: RootState) => state.game.currentQuesIndex,
+  );
+
+  const circleBg = currentQuesIndex > 3 ? 'var(--red)' : gameColor;
+  const circleEngaged =
+    currentQuesIndex > 3 ? 'var(--primary-color)' : 'var(--red)';
+
+  return (
+    <>
+      <span
+        css={css`
+          position: absolute;
+          content: '';
+          display: block;
+          top: 0;
+          left: 0;
+          width: ${size}px;
+          height: ${size}px;
+          border-radius: 100%;
+          transform: scale(1.05);
+          animation: 1.5s linear 0s infinite normal ${keyframes`
         0% {
-          background-image: conic-gradient(var(--primary-color) 0deg, #D94550 0deg);
+          background-image: conic-gradient(${circleEngaged} 0deg, ${circleBg} 0deg);
         }
         12.5% {
-          background-image: conic-gradient(var(--primary-color) 45deg, #D94550 45deg);
+          background-image: conic-gradient(${circleEngaged} 45deg, ${circleBg} 45deg);
         }
         25% {
-          background-image: conic-gradient(var(--primary-color) 90deg, #D94550 90deg);
+          background-image: conic-gradient(${circleEngaged} 90deg, ${circleBg} 90deg);
         }
         37.5% {
-          background-image: conic-gradient(var(--primary-color) 135deg, #D94550 135deg);
+          background-image: conic-gradient(${circleEngaged} 135deg, ${circleBg} 135deg);
         }
         50% {
-          background-image: conic-gradient(var(--primary-color) 180deg, #D94550 180deg);
+          background-image: conic-gradient(${circleEngaged} 180deg, ${circleBg} 180deg);
         }
         62.5% {
-          background-image: conic-gradient(var(--primary-color) 225deg, #D94550 225deg);
+          background-image: conic-gradient(${circleEngaged} 225deg, ${circleBg} 225deg);
         }
         75% {
-          background-image: conic-gradient(var(--primary-color) 270deg, #D94550 270deg);
+          background-image: conic-gradient(${circleEngaged} 270deg, ${circleBg} 270deg);
         }
         87.5% {
-          background-image: conic-gradient(var(--primary-color) 315deg, #D94550 315deg);
+          background-image: conic-gradient(${circleEngaged} 315deg, ${circleBg} 315deg);
         }
         100% {
-          background-image: conic-gradient(var(--primary-color) 360deg, #D94550 360deg);
+          background-image: conic-gradient(${circleEngaged} 360deg, ${circleBg} 360deg);
         }
       `};
-      `}
-    />
-    <span
-      css={css`
-        position: absolute;
-        content: '';
-        display: block;
-        top: 0;
-        left: 0;
-        width: ${size}px;
-        height: ${size}px;
-        border-radius: 100%;
-        background: var(--red);
-      `}
-      className="bigquestioncircle-red"
-    />
-  </>
-);
+        `}
+      />
+      <span
+        css={css`
+          position: absolute;
+          content: '';
+          display: block;
+          top: 0;
+          left: 0;
+          width: ${size}px;
+          height: ${size}px;
+          border-radius: 100%;
+          background: var(--red);
+        `}
+        className="bigquestioncircle-red"
+      />
+    </>
+  );
+};
 
 export default BigQuestionCircle;
